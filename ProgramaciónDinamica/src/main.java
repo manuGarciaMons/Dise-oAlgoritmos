ec1e4fdacee8de13336d56ed91a2d8c81fe58e1a
Manuela García M <42476682+manuGarciaMons@users.noreply.github.com>
2020-04-25 21:12:59 -0500

    finalizado

--- ProgramaciónDinamica/src/main.java
+++ ProgramaciónDinamica/src/main.java
@@ -1,29 +1,51 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
 
-	public static int mayorBeneficio(int[] valor, int tamaño) {
-		int[] solucion = new int[tamaño + 1];
-		solucion[0] = 0;
+    public static void main(String[] args) {
+        int[] precio = { 1,	5,	8,	9,	10,	17,	17,	20,	24,	30 };
+        System.out.println("Longitudes disponibles: 1,2,3,4,5,6,7,8,9,10");
+        System.out.println("Precios: 1,5,8,9,10,17,17,20,24,30");
+        Scanner entrada = new Scanner (System.in);
+		System.out.println("Ingrese el valor n (Longitud) de la varilla en un rango de 1-10");
+        System.out.println();
+        int tam = entrada.nextInt();
+        main r = new main();
+        r.maximoBeneficio(precio,tam);
+    }
 
-		for (int i = 1; i <= tamaño; i++) {
-			int max = -1;
-			for (int j = 0; j < i; j++) {
-				max = Math.max(max, valor[j] + solucion[i - (j + 1)]);
-				solucion[i] = max;
-			}
-		}
-		return solucion[tamaño];
-	}
+    public void maximoBeneficio(int[] precio, int tam){
+        int columnas = tam;
+        int filas = tam;
 
-	public static void main(String[] args) {
-		int[] value = { 1, 5, 8, 9, 10,17, 17, 20, 24,30 };
-		Scanner entrada = new Scanner (System.in);
-		System.out.println("Ingrese el valor n (Longitud) de la varilla");
-		int len = entrada.nextInt();
-		System.out.println("El mayor beneficio es " + len + ":"
-				+ mayorBeneficio(value, len));
+        int[][] matrix =  new int[filas+1][columnas+1];
+        for(int i=0; i <=filas; i++){
+            matrix[i][0] = 0;
+        }
+        for(int i=0; i <=columnas; i++){
+            matrix[0][i] = 0;
+        }
 
-	}
-
+        for(int i=1; i <=filas; i++){
+            for(int j=1; j<=columnas; j++) {
+                if (j >= i){
+                    matrix[i][j] = Math.max(matrix[i-1][j],precio[i-1]+matrix[i][j-i]);
+                }else{
+                    matrix[i][j] = matrix[i-1][j];
+                }
+            }
+        }
+        System.out.println("Matriz de Iteraciones");
+        for(int i=0; i <=filas; i++){
+            for(int j=0; j <=filas; j++) {
+                System.out.print(matrix[i][j]+" ");
+                
+            }
+           
+            System.out.println();
+        }
+       
+        System.out.println("El mayor beneficio para una logitud: "+tam+" es: "+matrix[filas][columnas]);
+    }
 }
